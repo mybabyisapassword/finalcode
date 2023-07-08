ec78014a82af1e8c0d2bde72176cb2b31764906f
mybabyisapassword <129019478+mybabyisapassword@users.noreply.github.com>
2023-07-08 12:44:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,152 +2,64 @@ const quizData = {
   "c": {
   "easy": [
     {
-    "question": "1.\n#include <stdio.h> \n __ main() { \n  printf(\"Hello World!\"); \n return 0; }\n\nCorrect answer:",
-    "choices": ["^", "<", "*", "="],
-    "correctAnswer": "<",
-    "output": "Hello World!"
-  },
-  {
-    "question": "2.\n#include <stdio.h> \n int main() { \n  printf(\"Hello students!\"); \n  printf(\"Im here to teach C programming.\"); \n return 0; }\n\nCorrect answer:",
-    "choices": ["+", ";","int"],
-    "correctAnswer": "+",
-    "output": "Hello students!Im here to teach C programming."
-  },
-  {
-    "question": "3.\n#include <stdio.h>\nint main() {\n  printf(\"Hello Teacher!____\");\n  printf(\"I am learning C.\\nAnd it is awesome!\");\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["\\m", "\\n", "And"],
-    "correctAnswer": "\\n",
-    "output": "Hello Teacher!\nI am learning C.\nAnd it is awesome!"
-  },
-  {
-    "question": "4.\n#include <stdio.h>\nint main() {\n  int myNum = 15;\n  printf(\"%d\", myNum);\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["%", "$", "&"],
-    "correctAnswer": "%",
-    "output": "15"
-  },
-  {
-    "question": "5.\n#include <stdio.h>\nint main() {\n  float my__Num = 3.5;\n  double myDoubleNum = 19.99;\n  printf(\"%f\\n\", myFloatNum);\n  printf(\"%If\", myDoubleNum);\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["Loaf", "Float", "Cokefloat"],
-    "correctAnswer": "Float",
-    "output": "3.500000\n19.990000"
-  },
-  {
-    "question": "6.\n#include <stdio.h>\nint main() {\n  int x = 5;\n  ______= 2;\n  int sum = 5 / 2;\n  printf(\"%d\", sum);\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["int y", "printf", "int y\nPrint y"],
-    "correctAnswer": "int y",
-    "output": "2"
-  },
-  {
-    "question": "7.\n____#include <stdio.h>\nint main() {\n  int myNum = 100 + 50;\n  printf(\"%d\", myNum);\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["%", "+", "#"],
-    "correctAnswer": "#",
-    "output": "150"
-  },
-  {
-    "question": "8.\n#include <stdio.h>\nint main() {\n  printf(\"%d\", 10 > 9); \n // ______ 1 (true) because 10 is greater than 9\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["1", "Returns", "true"],
-    "correctAnswer": "Returns",
-    "output": "1"
-  },
-  {
-    "question": "9.\n#include <stdio.h>\nint main() {\n  if (20 > 18) {\n    ______(\"20 is greater than 18\");\n  }\n  return 0;\n}\n\nCorrect answer:",
-    "choices": ["printf", "Println", "int main"],
-    "correctAnswer": "printf",
-    "output": "20 is greater than 18"
-  },
-  {
-    "question": "10.\n#include <stdio.h>\nint main() {\n  short a;\n  long b;\n\n\n  printf(\"1.a)______ of short = %d bytes\\n\", sizeof(a));\n  printf(\"size of long = %d 1.b)______\\n\", sizeof(b));\n  return 0;\n}\n\nCorrect answer:\n",
-    "choices": ["A.)\nSIZE\nsize\nSeize", "B.)\nbytes\nBits\nBeats"],
-    "correctAnswer": "A.)\nSIZE\nsize\nSeize\nB.)\nbytes\nBits\nBeats",
-    "output": "size of short = 2 bytes\nsize of long = 8 bytes"
-  },
+      "question": "#include <stdio.h>\n\nint main() {\n   printf(\"Hello World!\");\n   return 0;\n}\n\nWhat is the correct answer?",
+      "choices": ["^", "<", "*", "="],
+      "correctAnswer": "<",
+      "output": "Hello World!"
+    },
+    {
+      "question": "What is the keyword used to define a constant in C?",
+      "choices": ["final", "const",  "define"],
+      "correctAnswer": "const"
+    },
+    {
+      "question": "Which symbol is used to denote a single-line comment in C?",
+      "choices": ["//", "/*", "##", "--"],
+      "correctAnswer": "//"
+    },
+    {
+      "question": "What is the extension of C source code files?",
+      "choices": [".c", ".cpp", ".h", ".txt"],
+      "correctAnswer": ".c"
+    },
+    
+    {
+      "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int x = 10;\n   printf(\"%d\", x++);\n   return 0;\n}\n```\n",
+      "choices": ["9", "10", "11","12" ],
+      "correctAnswer": "10"
+    },
     
   ],
   "normal": [
-   {
-    "question": "1.\n#include <stdio.h>\nint main() 1.b)__\n  int 1.a)______ 15;\n  printf(\"%d\", myNum);\n  return 0;\n}\n\nCorrect answer\nmyNum =",
-    "choices": ["A.)\nMynum≠", "myNum =", "mynumber -"],
-    "correctAnswer": "myNum =",
-    "output": "15"
-  },
-  {
-    "question": "2.\n#include <stdio.h>\nint main() {\n    int number;\n    printf(\"Enter an integer: \");\n    1.a)______(\"%d\", &number);\n    // true if number is less than 0\n    if (number < 0) {\n        printf(\"You entered %d.1.b)______\", number);\n    }\n    printf(\"The if statement is easy.\"); for \n    return 0;\n}\n\nCorrect answer\nscanf\n\\n",
-    "choices": ["A.)\nprintf()", "scanf", "Println()"],
-    "correctAnswer": "scanf",
-    "output": "WHAT IS THE SECOND ONE ?"
-  },
-  {
-    "question": "2.\n#include <stdio.h>\nint main() {\n    int number;\n    printf(\"Enter an integer: \");\n    1.a)_scanf_(\"%d\", &number);\n    // true if number is less than 0\n    if (number < 0) {\n        printf(\"You entered %d.1.b)______\", number);\n    }\n    printf(\"The if statement is easy.\"); for \n    return 0;\n}\n\nCorrect answer\nscanf\n\\n",
-    "choices": ["B.)\n/N", "\\n", "%d"],
-    "correctAnswer": "\\n",
-    "output": "Enter an integer: 6\nThe if statement is easy."
-  },
-
-  {
-    "question": "3.\n#include <stdio.h>\nint main() {\n    int number1, number2;\n    printf(\"Enter two integers: \");\n    scanf(\"%d %d\", &number1, &number2);\n\n    if (number1 >= number2) {\n      if (number1 == number2) {\n        printf(\"Result: %d = %d\",number1,number2);\n      }\n      else {\n        printf(\"1.a)____> %d\", number1, number2);\n      }\n    }\n    else {\n        1.b)______(\"Result: %d < %d\",number1, number2);\n    }\n\n    return 0;\n}\n\nCorrect answer\nResult: %d ",
-    "choices": ["A.)\nresult:&d", "Result: %d", "Result"],
-    "correctAnswer": "Result: %d",
-    "output": "WHAT IS THE SECOND ONE ?"
-  },
-
-  {
-    "question": "3.\n#include <stdio.h>\nint main() {\n    int number1, number2;\n    printf(\"Enter two integers: \");\n    scanf(\"%d %d\", &number1, &number2);\n\n    if (number1 >= number2) {\n      if (number1 == number2) {\n        printf(\"Result: %d = %d\",number1,number2);\n      }\n      else {\n        printf(\"1.a)Result: %d>\", number1, number2);\n      }\n    }\n    else {\n        1.b)______(\"Result: %d < %d\",number1, number2);\n    }\n\n    return 0;\n}\n\nCorrect answer\nResult: %d ",
-    "choices": ["B.)\nprintf", "println", "Print();"],
-    "correctAnswer": "B.)\nprintf",
-    "output": "Result: 2 < 7"
-  },
-
-
-
-
-
-  {
-    "question": "// Print numbers from 1 to 10\n#include <stdio.h>\n\nint main() {\n  1.a)______\n  for (i = 1; i < 11; ++i)\n  {\n    printf(\"1.b)_____ \", i);\n  }\n  return 0;\n}\n\nCorrect answer:\nint i;\n%d",
-    "choices": ["A.)\nInt", "int ;", "int i;"],
-    "correctAnswer": "int i;",
-    "output": "WHAT IS THE SECOND ONE ?"
-  },
-
-
-  {
-    "question": "// Print numbers from 1 to 10\n#include <stdio.h>\n\nint main() {\n  1.a)__int i;__\n  for (i = 1; i < 11; ++i)\n  {\n    printf(\"1.b)_____ \", i);\n  }\n  return 0;\n}\n\nCorrect answer:\nint i;\n%d",
-    "choices": ["B.)\n%d", "%m", "=="],
-    "correctAnswer": "B.)\n%d",
-    "output": "1 2 3 4 5 6 7 8 9 10"
-  },
-
-
-
-
-  {
-    "question": "// Print numbers from 1 to 5\n\n#include <stdio.h>\nint main() {\n  int i = 1;\n    \n  1.a)_____ (i <= 5) {\n    printf(\"%d\\n\", i);\n1.b)_____  }\n\n  return 0;\n}\n\nCorrect answer:\nwhile\n    ++i;",
-    "choices": ["A.)\nWhile", "while", "When"],
-    "correctAnswer": "while",
-    "output": "WHAT IS THE SECODE ONE ?"
-  },
-{
-    "question": "// Print numbers from 1 to 5\n\n#include <stdio.h>\nint main() {\n  int i = 1;\n    \n  1.a)___while__ (i <= 5) {\n    printf(\"%d\\n\", i);\n1.b)_____  }\n\n  return 0;\n}\n\nCorrect answer:\nwhile\n    ++i;",
-    "choices": ["B.)\nI++:", "++i;", "j==;"],
-    "correctAnswer": "++i;",
-    "output": "1\n2\n3\n4\n5"
-  },
-
-
-
-
-
-  {
-    "question": "#include <stdio.h>\n\nint main() {\n  int my1.a)______= {25, 50, 75, 100};\n  int i;\n  \n  for (i = 0; i < 4; i++) {\n    printf(\"%d\\n\", 1.b)_______);\n  }\n \n  return 0;\n}\n\nCorrect answer\nNumbers[]",
-    "choices": ["A.)\nnumber()", "Numbers[]", "Num[]"],
-    "correctAnswer": "Numbers[]",
-    "output": "25\n50\n75\n100"
-  },
-{
-    "question": "#include <stdio.h>\n\nint main() {\n  int my1.a)___Numbers[]___= {25, 50, 75, 100};\n  int i;\n  \n  for (i = 0; i < 4; i++) {\n    printf(\"%d\\n\", 1.b)_______);\n  }\n \n  return 0;\n}\n\nCorrect answer\nNumbers[]",
-    "choices": ["B.)\nMynumber()", "myNumbers[i]", "Int num()"],
-    "correctAnswer": "myNumbers[i]",
-    "output": "25\n50\n75\n100"
-  },
+    {
+      "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int x = 5;\n   int *p = &x;\n   printf(\"%d\", *p);\n   return 0;\n}\n```\n",
+      "choices": ["5", "0", "Garbage value"],
+      "correctAnswer": "5"
+    },
+    
+    {
+      "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int arr[] = {1, 2, 3, 4, 5};\n   printf(\"%d\", *(arr + 2));\n   return 0;\n}\n```\n",
+      "choices": ["1", "2", "3", "4"],
+      "correctAnswer": "3"
+    },
+   
+    {
+      "question": "What is the result of the following expression in C?\n\n```c\n3 + 5 * 2 - 4 / 2\n```\n",
+      "choices": ["6", "10", "9","7"],
+      "correctAnswer": "10"
+    },
+    {
+      "question": "Which of the following is NOT a valid data type in C?",
+      "choices": ["float", "boolean", "char",],
+      "correctAnswer": "boolean"
+    },
+    
+    {
+      "question": "Which of the following statements is used to terminate a loop in C?",
+      "choices": ["break", "stop", "exit", ],
+      "correctAnswer": "break"
+    },
+   
     
   ],
   "hard": [
@@ -158,12 +70,12 @@ const quizData = {
     },
     {
       "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int arr[5];\n   printf(\"%d\", sizeof(arr));\n   return 0;\n}\n```\n",
-      "choices": ["5", "20", "4" ],
+      "choices": ["5", "20", "4", ],
       "correctAnswer": "20"
     },
     {
       "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int arr[5] = {1, 2, 3, 4, 5};\n   int *p = &arr[1];\n   printf(\"%d\", *(p + 2));\n   return 0;\n}\n```\n",
-      "choices": ["1", "2", "4","3"],
+      "choices": ["1", "2", "4","3",],
       "correctAnswer": "4"
     },
     {
@@ -173,7 +85,7 @@ const quizData = {
     },
     {
       "question": "What is the output of the following code?\n\n```c\n#include <stdio.h>\n\nint main() {\n   int x = 5;\n   printf(\"%d\", x++ + x++);\n   return 0;\n}\n```\n",
-      "choices": ["10", "11", "Error" ],
+      "choices": ["10", "11", "Error", ],
       "correctAnswer": "11"
     },
     
@@ -184,25 +96,56 @@ const quizData = {
   "easy": [
     
     {
-      "question": "What is the correct HTML tag for the largest heading?",
-      "choices": ["<h1>",  "<head>", "<heading>"],
-      "correctAnswer": "<h1>"
-    },
-    {
-      "question": "Which HTML attribute is used to define inline styles?",
-      "choices": ["style", "class", "id", "font"],
-      "correctAnswer": "style"
-    },
-    {
-      "question": "Which HTML tag is used to define an unordered list?",
-      "choices": ["<ul>", "<ol>", "<li>", "<list>"],
-      "correctAnswer": "<ul>"
-    },
-    {
-      "question": "What is the correct HTML tag for inserting a line break?",
-      "choices": ["<br>", "<break>", "<lb>", "<newline>"],
-      "correctAnswer": "<br>"
-    }
+    question: '1. <____DOCTYPE html>\n<html>\n    <head>\n    <title>First Web Page</title>\n</head>\n<body>Hello World!</body>\n</html>\nOutput:\nHello World!\nCorrect Answer: !\nChoices:\n?\n!\n#',
+    answer: '!',
+    choices: ['?', '!', '#'],
+    Output:"Hello World!"
+  },
+  {
+    question: '2. html document\n<!DOCTYPE html>\n<html>\n<head>\n<title>Page Title</title>\n</head>\n<body>\n<h1>The Car<____ >\n<p>The Car type is honda and its color red.</p>\n</body>\n</html>\nOutput:\nThe Car\nThe Car type is honda and its color red.\nCorrect answer: /h1\nChoices:\n/H1\nh1\n/h1',
+    answer: '/h1',
+    choices: ['/H1', 'h1', '/h1']
+  },
+  {
+    question: '3. html links\n<!DOCTYPE html>\n<html>\n<body>\n<h2> HTML _____</h2>\n<a href="https://www.identicode.com">\nidenticode</a>\n</body>\n</html>\nOutput:\nHTML Links\nidenticode\nCorrect Answer: Links\nChoices:\n“Links”\nlinks\nLinks',
+    answer: 'Links',
+    choices: ['“Links”', 'links', 'Links']
+  },
+  {
+    question: '4. html image\n<!DOCTYPE html>\n<html>\n<body>\n<h2> HTML Images</h2>\n<img src= "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/A-Cat.jpg/1200px-A-Cat.jpg?20101227100718"\n_______="300"\nheight="200.75">\n</body>\n</html>\nOutput:\nHTML Images\n\nCorrect Answer: width\nChoices:\n/width\nwidth\nWidth',
+    answer: 'width',
+    choices: ['/width', 'width', 'Width']
+  },
+  {
+    question: '5. html image\n<!DOCTYPE html>\n<html>\n<body>\n\n<h2>HTML Image</h2>\n<img src__"pic_trulli.jpg" alt="Trulli" \nwidth="500" \nheight="333">\n\n</body>\n</html>\nOutput:\nHTML Image\n\nCorrect Answer: =\nChoices:\n=\n:\n-',
+    answer: '=',
+    choices: ['=', ':', '-']
+  },
+  {
+    question: '6. html Button\n<!DOCTYPE html>\n<html>\n<body>\n<h2>HTML Buttons</h2>\n<p>HTML buttons are defined with the button tag:</p>\n<button>Click me<______ >\n</body>\n</html>\nOutput:\nHTML Buttons\nHTML buttons are defined with the button tag:\n(Click me)\nCorrect answer: /button\nChoices:\n/button\nButton\nButton',
+    answer: '/button',
+    choices: ['/button', 'Button', 'Button']
+  },
+  {
+    question: '<!DOCTYPE>\n<html>\n<body>\n<h2>An Unordered HTML List</h2>\n<ul>\n  <li>Coffee</li>\n  <li>Tea</li>\n  <li>Milk<___>\n</ul>  \n</body>\n</html>\nOutput:\nAn Unordered HTML List\nCoffee\nTea\nMilk\nCorrect Answer: /li\nChoices:\n/Li\nli\n/li',
+    answer: '/li',
+    choices: ['/Li', 'li', '/li']
+  },
+  {
+    question: '<!DOCTYPE html>\n<html>\n<body>\n\n<!-- This is a comment -->\n______This is a paragraph.</p>\n<!-- Comments are not displayed in the browser -->\n\n</body>\n</html>\nOutput:\nThis is a paragraph\n\nCorrect answer:\n<p>\nChoices:\n<h1>\n</h>\n<p>',
+    answer: '<p>',
+    choices: ['<h1>', '</h>', '<p>']
+  },
+  {
+    question: '<!DOCTYPE html>\n<html>\n<body>\n<h2>HTML Links</h2>\n<p>HTML links are defined with the a tag:</p>\n_________"https://www.Identicode.com">Identicode</a>\n</body>\n</html>\n\nOutput:\nHTML Links\nHTML links are defined with the a tag:\n\nIdenticode\nCorrect answer:\n<a href=\nChoices :\nhref=\n<a href=\na href =',
+    answer: '<a href=',
+    choices: ['href=', '<a href=', 'a href =']
+  },
+  {
+    question: '10.html text size\n\n<!DOCTYPE html>\n<html>\n<body>\n\n<h1 style="font-size:300%;">This is a heading</h1>\n<p style="font-size:160_____;">This is a paragraph.</p>\n\n</body>\n</html>\nOutput:\nThis is a heading\nThis is a paragraph.\nCorrect answer: %\nChoices:\n&\n@\n%',
+    answer: '%',
+    choices: ['&', '@', '%']
+  }
   ],
   "normal": [
     {
@@ -243,7 +186,7 @@ const quizData = {
     },
     {
       "question": "What is the output of the following code?\n\n#include <iostream>\n\nint main() {\n  int x = 5;\n  std::cout << x;\n  return 0;\n}",
-      "choices": ["5", "x", "std::cout"],
+      "choices": ["5", "x", "std::cout", "Compilation error"],
       "correctAnswer": "5"
     },
     {
@@ -321,7 +264,7 @@ const quizData = {
 
 
   "js": {
-      "easy": [
+  "easy": [
     {
       "question": "Which keyword is used to declare a variable in JavaScript?",
       "choices": ["var", "int", "variable"],
@@ -342,7 +285,7 @@ const quizData = {
       "question": "Which operator is used to concatenate strings in JavaScript?",
       "choices": ["+", "*", "-", "/"],
       "correctAnswer": "+"
-    },
+    }
   ],
   "normal": [
     {
@@ -366,7 +309,7 @@ const quizData = {
       "question": "What is the output of the following code?\n\nconsole.log(3 === '3');",
       "choices": ["true", "false", "NaN"],
       "correctAnswer": "false"
-    },
+    }
   ],
   "hard": [
     {
@@ -387,11 +330,11 @@ const quizData = {
     },
     {
       "question": "What is the output of the following code?\n\nconsole.log(5 + true);",
-      "choices": ["6", "5", "TypeError" ],
+      "choices": ["6", "5", "TypeError", ],
       "correctAnswer": "6"
     }
   ],
-},  
+},
 
   "java": {
   "easy": [
@@ -608,9 +551,9 @@ function showQuestion() {
     choice.value = currentQuestion.choices[i];
 
     choice.style.display = "inline-block"; // Set display to "inline-block" to align buttons horizontally
-    choice.style.marginRight = "20px"; // Add some margin between buttons
-    choice.style.padding = "15px";
-    choice.style.fontSize = "20px";
+    choice.style.marginRight = "10px"; // Add some margin between buttons
+    choice.style.padding = "25px";
+    choice.style.fontSize = "35px";
     choice.style.backgroundColor = "white";
     choice.style.color = "black";
     choice.style.border = "none";
@@ -631,13 +574,13 @@ function checkAnswer(event) {
 
   if (selectedAnswer === currentQuestion.correctAnswer) {
     resultElement.innerHTML = "Correct! <br>" + "output: &nbsp&nbsp" + currentQuestion.output;
-    resultElement.style.fontSize = "20px";
+    resultElement.style.fontSize = "30px";
     resultElement.style.color = "#39FF14";
     resultElement.style.fontFamily = "rockwell";
     score++;
 
     // Add glow effect
-    resultElement.style.textShadow = "0 0 15px #39FF14, 0 0 20px red, 0 0 20px blue";
+    resultElement.style.textShadow = "0 0 10px #39FF14, 0 0 20px #39FF14, 0 0 30px #39FF14";
   } else {
     resultElement.textContent = "Incorrect!";
     resultElement.style.fontSize = "30px";
@@ -681,3 +624,4 @@ function endGame() {
     location.reload();
   }, 5000);
 }
+
